328dc6486bf071c94f7b5969e0423087b40cdb52
abdulshakur <abdulshakur486@gmail.com>
2021-11-29 14:27:21 +0300

    changed url path to production instead of localhost

--- src/components/history/Histories.js
+++ src/components/history/Histories.js
@@ -30,7 +30,7 @@ function Histories() {
       axios
         // https://audiophile-e-commerce.herokuapp.com
         // http://localhost:5010
-        .get("http://localhost:5010/history/all", {
+        .get("https://audiophile-e-commerce.herokuapp.com/history/all", {
           method: "GET",
           cancelToken: source.token,
         })
