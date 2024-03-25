f3ff0ddf2fed5ac0b9107b2a70ad0b6a8676b656
Abdulshakur <ashakur.js24@gmail.com>
2024-03-25 19:52:06 +0300

    edited

--- src/components/carts/Payment.js
+++ src/components/carts/Payment.js
@@ -4,7 +4,6 @@ import axios from "../../axios";
 import { useDispatch, useSelector } from "react-redux";
 import { openOrCloseCheckoutModal } from "../../app-redux/features/Dialogs";
 import TextField from "@material-ui/core/TextField";
-import { ThemeProvider } from "@material-ui/styles";
 import { LoadingButton } from "@mui/lab";
 import { toast } from "react-toastify";
 
@@ -139,97 +138,95 @@ function Payment({ classes, theme }) {
 
   return (
     <div className="payment">
-      <ThemeProvider theme={theme}>
-        <h6 onClick={() => openModal()}>BILLING DETAILS</h6>
-        <form id="checkout_form" onSubmit={handleSubmit}>
-          <div className="billings flex">
-            <TextField
-              className={`${classes} a`}
-              label="Name"
-              variant="outlined"
-              id="mui-theme-provider-outlined-input"
-              onChange={handleChangeField("name")}
-              value={name}
-              type="text"
-              required
-            />
-            <TextField
-              className={`${classes} b`}
-              label="Email Address"
-              variant="outlined"
-              id="mui-theme-provider-outlined-input"
-              onChange={handleChangeField("email")}
-              value={email}
-              type="email"
-              required
-            />
+      <h6 onClick={() => openModal()}>BILLING DETAILS</h6>
+      <form id="checkout_form" onSubmit={handleSubmit}>
+        <div className="billings flex">
+          <TextField
+            className={`${classes} a`}
+            label="Name"
+            variant="outlined"
+            id="mui-theme-provider-outlined-input"
+            onChange={handleChangeField("name")}
+            value={name}
+            type="text"
+            required
+          />
+          <TextField
+            className={`${classes} b`}
+            label="Email Address"
+            variant="outlined"
+            id="mui-theme-provider-outlined-input"
+            onChange={handleChangeField("email")}
+            value={email}
+            type="email"
+            required
+          />
 
-            <TextField
-              className={`${classes} c`}
-              label="Phone Number"
-              variant="outlined"
-              id="mui-theme-provider-outlined-input"
-              onChange={handleChangeField("phone")}
-              value={phone}
-              type="phone"
-              required
-            />
-          </div>
-          <h6>SHIPPING INFO</h6>
-          <div className="shipping">
-            <TextField
-              className={`${classes} a`}
-              label="Your Address"
-              variant="outlined"
-              id="mui-theme-provider-outlined-input"
-              onChange={handleChangeField("address")}
-              value={address}
-              type="address"
-              required
-            />
-            <TextField
-              className={`${classes} b`}
-              label="Zip Code"
-              variant="outlined"
-              id="mui-theme-provider-outlined-input"
-              onChange={handleChangeField("zipCode")}
-              value={zipCode}
-              type="text"
-              required
-            />
-            <TextField
-              className={`${classes} c`}
-              label="City"
-              variant="outlined"
-              id="mui-theme-provider-outlined-input"
-              onChange={handleChangeField("city")}
-              value={city}
-              type="text"
-              required
-            />
-            <TextField
-              className={`${classes} d`}
-              label="Country"
-              variant="outlined"
-              id="country"
-              onChange={handleChangeField("country")}
-              value={country}
-              type="text"
-              required
-            />
-          </div>
-          <h6>PAYMENT DETAILS</h6>
-          <CardElement onChange={handleChange} />
-          <LoadingButton
-            loading={processing}
-            disabled={processing || !complete || !cartsArr.length}
-            className="button"
-            type="submit"
-          >
-            {cartsArr.length ? "CONTINUE & PAY" : "Your Cart is Empty!"}
-          </LoadingButton>
-        </form>
-      </ThemeProvider>
+          <TextField
+            className={`${classes} c`}
+            label="Phone Number"
+            variant="outlined"
+            id="mui-theme-provider-outlined-input"
+            onChange={handleChangeField("phone")}
+            value={phone}
+            type="phone"
+            required
+          />
+        </div>
+        <h6>SHIPPING INFO</h6>
+        <div className="shipping">
+          <TextField
+            className={`${classes} a`}
+            label="Your Address"
+            variant="outlined"
+            id="mui-theme-provider-outlined-input"
+            onChange={handleChangeField("address")}
+            value={address}
+            type="address"
+            required
+          />
+          <TextField
+            className={`${classes} b`}
+            label="Zip Code"
+            variant="outlined"
+            id="mui-theme-provider-outlined-input"
+            onChange={handleChangeField("zipCode")}
+            value={zipCode}
+            type="text"
+            required
+          />
+          <TextField
+            className={`${classes} c`}
+            label="City"
+            variant="outlined"
+            id="mui-theme-provider-outlined-input"
+            onChange={handleChangeField("city")}
+            value={city}
+            type="text"
+            required
+          />
+          <TextField
+            className={`${classes} d`}
+            label="Country"
+            variant="outlined"
+            id="country"
+            onChange={handleChangeField("country")}
+            value={country}
+            type="text"
+            required
+          />
+        </div>
+        <h6>PAYMENT DETAILS</h6>
+        <CardElement onChange={handleChange} />
+        <LoadingButton
+          loading={processing}
+          disabled={processing || !complete || !cartsArr.length}
+          className="button"
+          type="submit"
+        >
+          {cartsArr.length ? "CONTINUE & PAY" : "Your Cart is Empty!"}
+        </LoadingButton>
+      </form>
     </div>
   );
 }
